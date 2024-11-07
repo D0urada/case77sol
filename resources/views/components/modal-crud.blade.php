062a6a865acaa499c1641295f54ba5376c8873a7
D0urada <mariavc.dourado@gmail.com>
2024-11-07 05:42:54 -0300

    fix: inputs colors

--- resources/views/components/modal-crud.blade.php
+++ resources/views/components/modal-crud.blade.php
@@ -51,7 +51,7 @@ $maxWidth = [
 >
     <div
         x-show="show"
-        class="fixed inset-0 transform transition-all"
+        class="fixed inset-0 transition-all transform"
         x-on:click="show = false"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0"
@@ -60,12 +60,12 @@ $maxWidth = [
         x-transition:leave-start="opacity-100"
         x-transition:leave-end="opacity-0"
     >
-        <div class="absolute inset-0 bg-gray-500 dark:bg-gray-900 opacity-75"></div>
+        <div class="absolute inset-0 bg-gray-700 opacity-75 dark:bg-gray-900"></div>
     </div>
 
     <div
         x-show="show"
-        class="mt-16 overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto bg-white rounded-lg shadow dark:bg-gray-700"
+        class="mt-16 overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto bg-white rounded-lg shadow dark:bg-gray-800"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
@@ -74,7 +74,7 @@ $maxWidth = [
         x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
     >
 		@isset($modalHeader)
-			<div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+			<div class="flex items-center justify-between p-4 border-b rounded-t md:p-5 dark:border-gray-600">
 				<h3 class="text-lg font-semibold text-gray-900 dark:text-white">
 					{{ $modalHeader }}
 				</h3>
